ce1d8018008afdbb003569e1756fae35f722f152
MOHAMMED MAAZ <63781208+maaz19499@users.noreply.github.com>
2023-07-18 09:43:32 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,13 +15,16 @@ class MyBot(ActivityHandler):
     ):
         for member_added in members_added:
             if member_added.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("Hi, This is your Custom LLM Bot!")
+                await turn_context.send_activity("Hi Welcome to ACloudz.AI, Your one stop destination for all the services!")
 
     async def on_message_activity(self, turn_context: TurnContext):
         # await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
-        greeting_list = ['Hi','Hello','HI','hi','Hey','HEY','hey','hello'] 
+        greeting_list = ['Hi','Hello','HI','hi','Hey','HEY','hey','hello']
+        exit_list = ['Bye','bye','exit','quit','goodbye','Goodbye']
         if turn_context.activity.text in greeting_list:
-            answer= 'Hello, how can I help you?' 
+            answer= 'Hello, how can I help you?'
+        elif turn_context.activity.text in exit_list:
+            answer= 'Bye, have a Good Day!'
         else:
             answer = get_answers(turn_context.activity.text)  
         return await turn_context.send_activity(MessageFactory.text(answer))
